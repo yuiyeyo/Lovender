75994ea8384857f44b02373defea69e7119f6bd5
Aurielle Li <89785862+yuiyeyo@users.noreply.github.com>
2023-12-22 23:56:30 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const topImages = document.querySelectorAll('.topslideshow a');
-
-  // Initialize FancyBox
-  Fancybox.bind(topImages, {
-    loop: true,
-  });
-
+  const topSlide = document.getElementById('topslide');
+  const images = topSlide.querySelectorAll('img');
   let currentIndex = 0;
 
   function changeSlide() {
-    currentIndex = (currentIndex + 1) % topImages.length;
-    Fancybox.open(topImages, currentIndex);
+    images[currentIndex].style.opacity = 0;
+    currentIndex = (currentIndex + 1) % images.length;
+    images[currentIndex].style.opacity = 1;
   }
 
   // Change slide every second
